5d3da48b5621b6480a7c8117231c14f89fce2acf
MukeshBabu06 <mukesh.shaankar@gmail.com>
2024-08-22 18:17:51 +0530

    Add files via upload

---
+++ React Grid and Flex Image Gallery/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Gallery from "./Mycomp/PrjImageGallery.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
+}
+export default App;
---
+++ React Grid and Flex Image Gallery/src/Random Images/PrjImageGallery.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Prjgallery.css";
+
+export default function Gallery(){
+    return(
+        <body>
+            <h1>React Image Gallery (Grid Layout)</h1>
+            <div class="Gallery">
+                    <div className="img1"><img src="https://picsum.photos/200/200?random=1" alt="" /></div>
+                    <div className="img2"><img src="https://picsum.photos/200/200?random=2" alt=""/></div>
+                    <div className="img3"><img src="https://picsum.photos/200/200?random=3" alt=""/></div>
+                    <div className="img4"><img src="https://picsum.photos/200/200?random=4" alt=""/></div>
+                    <div className="img5"><img src="https://picsum.photos/200/200?random=5" alt=""/></div>
+                    <div className="img6"><img src="https://picsum.photos/200/200?random=6" alt=""/></div>
+                    <div className="img7"><img src="https://picsum.photos/200/200?random=7" alt=""/></div>
+                    <div className="img8"><img src="https://picsum.photos/200/200?random=8" alt=""/></div>
+                    <div className="img9"><img src="https://picsum.photos/200/200?random=9" alt=""/></div>
+                    <div className="img10"><img src="https://picsum.photos/200/200?random=10" alt=""/></div>
+            </div>
+            <br />
+            <h1>React Image Gallery (Flex Layout)</h1>
+            <div class="Gallery1">
+                    <div><img src="https://picsum.photos/200/200?random=11" alt="" /></div>
+                    <div><img src="https://picsum.photos/200/200?random=12" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=13" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=14" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=15" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=16" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=17" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=18" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=19" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=20" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=21" alt="" /></div>
+                    <div><img src="https://picsum.photos/200/200?random=22" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=23" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=24" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=25" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=26" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=27" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=28" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=29" alt=""/></div>
+                    <div><img src="https://picsum.photos/200/200?random=30" alt=""/></div>
+            </div>
+        </body>
+    )
+}
